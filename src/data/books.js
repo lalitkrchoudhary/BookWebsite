e09be352efd3461ce5b8f8386e4bffe904ae6010
Lalit Choudhary <lalitchoudhary@Lalits-MacBook-Air.local>
2026-03-13 04:15:59 +0530

    added new data

--- src/data/books.js
+++ src/data/books.js
@@ -1,51 +1,92 @@
-
-export const books_data=[
+export const books_data = [
   {
-    "id": 1,
-    "title": "The Alchemist",
-    "author": "Paulo Coelho",
-    "image": "https://covers.openlibrary.org/b/id/8231996-L.jpg"
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    image: "https://covers.openlibrary.org/b/id/8231996-L.jpg"
   },
   {
-    "id": 2,
-    "title": "Atomic Habits",
-    "author": "James Clear",
-    "image": "https://covers.openlibrary.org/b/id/11153243-L.jpg"
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    image: "https://covers.openlibrary.org/b/id/11153243-L.jpg"
   },
   {
-    "id": 3,
-    "title": "Rich Dad Poor Dad",
-    "author": "Robert T. Kiyosaki",
-    "image": "https://covers.openlibrary.org/b/id/8091016-L.jpg"
+    id: 3,
+    title: "Rich Dad Poor Dad",
+    author: "Robert T. Kiyosaki",
+    image: "https://covers.openlibrary.org/b/id/8091016-L.jpg"
   },
   {
-    "id": 4,
-    "title": "Think and Grow Rich",
-    "author": "Napoleon Hill",
-    "image": "https://covers.openlibrary.org/b/id/8228691-L.jpg"
+    id: 4,
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    image: "https://covers.openlibrary.org/b/id/8228691-L.jpg"
   },
   {
-    "id": 5,
-    "title": "The Psychology of Money",
-    "author": "Morgan Housel",
-    "image": "https://covers.openlibrary.org/b/id/10594765-L.jpg"
+    id: 5,
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    image: "https://covers.openlibrary.org/b/id/10594765-L.jpg"
   },
   {
-    "id": 6,
-    "title": "Deep Work",
-    "author": "Cal Newport",
-    "image": "https://covers.openlibrary.org/b/id/9259251-L.jpg"
+    id: 6,
+    title: "Deep Work",
+    author: "Cal Newport",
+    image: "https://covers.openlibrary.org/b/id/9259251-L.jpg"
   },
   {
-    "id": 7,
-    "title": "Start With Why",
-    "author": "Simon Sinek",
-    "image": "https://covers.openlibrary.org/b/id/8775116-L.jpg"
+    id: 7,
+    title: "Start With Why",
+    author: "Simon Sinek",
+    image: "https://covers.openlibrary.org/b/id/8775116-L.jpg"
   },
   {
-    "id": 8,
-    "title": "The 4-Hour Workweek",
-    "author": "Timothy Ferriss",
-    "image": "https://covers.openlibrary.org/b/id/8167890-L.jpg"
+    id: 8,
+    title: "The 4-Hour Workweek",
+    author: "Timothy Ferriss",
+    image: "https://covers.openlibrary.org/b/id/8167890-L.jpg"
+  },
+  {
+    id: 9,
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
+    image: "https://covers.openlibrary.org/b/id/240726-L.jpg"
+  },
+  {
+    id: 10,
+    title: "The Subtle Art of Not Giving a F*ck",
+    author: "Mark Manson",
+    image: "https://covers.openlibrary.org/b/id/8370226-L.jpg"
+  },
+  {
+    id: 11,
+    title: "Ikigai",
+    author: "Hector Garcia & Francesc Miralles",
+    image: "https://covers.openlibrary.org/b/id/9254903-L.jpg"
+  },
+  {
+    id: 12,
+    title: "Can't Hurt Me",
+    author: "David Goggins",
+    image: "https://covers.openlibrary.org/b/id/10076662-L.jpg"
+  },
+  {
+    id: 13,
+    title: "Zero to One",
+    author: "Peter Thiel",
+    image: "https://covers.openlibrary.org/b/id/8375042-L.jpg"
+  },
+  {
+    id: 14,
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    image: "https://covers.openlibrary.org/b/id/8375041-L.jpg"
+  },
+  {
+    id: 15,
+    title: "The Lean Startup",
+    author: "Eric Ries",
+    image: "https://covers.openlibrary.org/b/id/7222246-L.jpg"
   }
-]
+];
